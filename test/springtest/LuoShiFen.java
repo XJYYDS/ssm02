4a73f8ffe0cdc73dc769b2e25b6dabe15dc3bb2e
jlb <jlb0929@hotmail.com>
2021-08-26 23:11:55 +0800

    ssm

---
+++ test/springtest/LuoShiFen.java
@@ -0,0 +1,11 @@
+package springtest;
+
+public class LuoShiFen implements EatDongXi {
+    public LuoShiFen(){
+        System.out.println("辣条的对象");
+    }
+
+    public void eat(){
+        System.out.println("吃辣条");
+    }
+}
